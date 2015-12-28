1b2d6d4bf5a55d52eefc189949e80120d3b46e59
duodiscus92 <jacques.ehrlich@neuf.fr>
2015-12-28 09:29:55 +0100

    ajout d'un chemin et d'une action de multiplication

--- SimpleStockBundle/Controller/MonPremierController.php
+++ SimpleStockBundle/Controller/MonPremierController.php
@@ -4,11 +4,25 @@ namespace SYM16\SimpleStockBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class MonPremierController extends Controller
+/*class MonPremierController extends Controller
 {
     public function iLikeAction(){
 	$content = $this->get('templating')->render
 		('SYM16SimpleStockBundle:MonPremier:iLike.html.twig');
         return new Response($content);
+    }
+}*/
+
+class MonPremierController extends Controller
+{
+    public function iLikeAction($un_verbe_a_l_infinitif){
+        return new Response("J'aime beaucoup ".$un_verbe_a_l_infinitif.' !');
+    }
+
+    public function produitAction($multiplicande, $multiplicateur){
+	$produit = $multiplicande * $multiplicateur;
+	return new Response
+	   ("Le produit de " . $multiplicande . " par ". $multiplicateur. 
+		" est égale à : ". $produit); 
     }
 }
