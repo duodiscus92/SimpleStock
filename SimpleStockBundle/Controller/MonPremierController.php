a7f6166e748a257a64871453ba0be65dffbc4960
duodiscus92 <jacques.ehrlich@neuf.fr>
2015-12-29 09:57:10 +0100

    ajout produit et puissance

--- SimpleStockBundle/Controller/MonPremierController.php
+++ SimpleStockBundle/Controller/MonPremierController.php
@@ -3,6 +3,7 @@
 namespace SYM16\SimpleStockBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 /*class MonPremierController extends Controller
 {
@@ -18,11 +19,35 @@ class MonPremierController extends Controller
     public function iLikeAction($un_verbe_a_l_infinitif){
         return new Response("J'aime beaucoup ".$un_verbe_a_l_infinitif.' !');
     }
-
+/*
     public function produitAction($multiplicande, $multiplicateur){
-	$produit = $multiplicande * $multiplicateur;
+	$produit = hexdec($multiplicande) * hexdec($multiplicateur);
 	return new Response
 	   ("Le produit de " . $multiplicande . " par ". $multiplicateur. 
 		" est égale à : ". $produit); 
+    }
+*/
+    // produit de deux nombres
+    public function produitAction($multiplicande, $multiplicateur){
+	$produit = hexdec($multiplicande) * hexdec($multiplicateur);
+	return $this->render(
+		'SYM16SimpleStockBundle:MonPremier:produit.html.twig',
+		array('multiplicande' => $multiplicande,
+		      'multiplicateur' => $multiplicateur,
+		      'resultat' => $produit)
+        );
+    }
+
+    //elevation a une puissance
+    public function puissanceAction(Request $request) {
+	$valeur = $request->query->get('valeur');
+	$exposant = $request->query->get('exposant');
+	$resultat =  pow($valeur, $exposant);
+	return $this->render(
+		'SYM16SimpleStockBundle:MonPremier:puissance.html.twig',
+		array('valeur' => $valeur,
+		      'exposant' => $exposant,
+		      'resultat' => $resultat)
+        );
     }
 }
