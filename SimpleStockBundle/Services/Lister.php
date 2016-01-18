0db4c63c40132b2bd94264c51d751c27edde0521
duodiscus92 <jacques.ehrlich@neuf.fr>
2016-01-18 11:45:06 +0100

    mise à jour  du service

--- SimpleStockBundle/Services/Lister.php
+++ SimpleStockBundle/Services/Lister.php
@@ -28,31 +28,11 @@ class Lister
 	$listColnames  = $liste['listcolnames'];
 	// valeurs des entité à lister
 	$entities = $liste['entities'];
-	if(null == $entities){
-		throw new NotFoundHttpException("La liste n'existe pas");
-	}
-	$listEntities = array();
-	//boucle sur les lignes de la table
-	foreach ($entities as $entitie){
-		// chaque ligne est elle meme un tableau d'articles qu'on obtient avec les getters
-		$listArticle = array();
-		foreach ($listColnames as $key => $fieldname){
-		    //on reconstruite les getters à partir des noms de colonnes
-		    $fieldname = 'get'.$fieldname;
-		    // traitement special de l'id
-		    if ($key == 'id')
-			$listArticle['id'] = $this->AnyTypeToString($entitie->$fieldname());
-		    else
-		    	array_push($listArticle, $this->AnyTypeToString($entitie->$fieldname()));
-		}
-		// listeEntities est un tableau de tableau crée dynamiquement (on sait pas a priori le nombre de lignes)
-		array_push($listEntities, $listArticle);
-		unset($listArticle);
-	}
-	// construction des autres lignes
-	$path = $liste['path'];
-	// construction	 d'informations globales aux entités (ex : nombre de lignes de la liste, total TTC
-	$totaluser = $liste['totalusers'];
+	//
+	//
+	// à compléter
+	//
+	//
 	// lister !
 	return array(
 		// vue à utiliser pour lister
