f342f10b849008cd287ba56931fcf5262d1a79e1
Jacques Ehrlich <jacques.ehrlich@neuf.fr>
2016-02-01 10:01:33 +0100

    ajout filtre sur affichage reference reference (en cours)

---
+++ SimpleStockBundle/Entity/ReferenceFiltre.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace SYM16\SimpleStockBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ReferenceFiltre
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="SYM16\SimpleStockBundle\Entity\ReferenceFiltreRepository")
+ */
+class ReferenceFiltre
+{
+    const FILTRE_TOUS = 1;
+    const FILTRE_INCLURE = 2;
+    const FILTRE_EXCLURE = 3;
+
+    public function __construct()
+    {
+	$this->entrepotfiltre =
+	$this->categoriefiltre =
+	$this->createurfiltre =
+	$this->refvidefiltre = FILTRE_TOUS;
+    }
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="refvidefitre", type="smallint")
+     */
+    private $refvidefitre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="entrepot", type="string", length=255)
+     */
+    private $entrepot;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="entrepotfiltre", type="smallint")
+     */
+    private $entrepotfiltre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="categorie", type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="categoriefiltre", type="smallint")
+     */
+    private $categoriefiltre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="createur", type="string", length=255)
+     */
+    private $createur;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="createurfiltre", type="smallint")
+     */
+    private $createurfiltre;
+
+
+    /**
+     * Set refvide
+     *
+     * @param integer $refvide
+     * @return ReferenceFiltre
+     */
+    public function setRefvide($refvide)
+    {
+        $this->refvide = $refvide;
+
+        return $this;
+    }
+
+    /**
+     * Get refvide
+     *
+     * @return integer 
+     */
+    public function getRefvide()
+    {
+        return $this->refvide;
+    }
+
+    /**
+     * Set entrepot
+     *
+     * @param string $entrepot
+     * @return ReferenceFiltre
+     */
+    public function setEntrepot($entrepot)
+    {
+        $this->entrepot = $entrepot;
+
+        return $this;
+    }
+
+    /**
+     * Get entrepot
+     *
+     * @return string 
+     */
+    public function getEntrepot()
+    {
+        return $this->entrepot;
+    }
+
+    /**
+     * Set entrepotfiltre
+     *
+     * @param integer $entrepotfiltre
+     * @return ReferenceFiltre
+     */
+    public function setEntrepotfiltre($entrepotfiltre)
+    {
+        $this->entrepotfiltre = $entrepotfiltre;
+
+        return $this;
+    }
+
+    /**
+     * Get entrepotfiltre
+     *
+     * @return integer 
+     */
+    public function getEntrepotfiltre()
+    {
+        return $this->entrepotfiltre;
+    }
+
+    /**
+     * Set categorie
+     *
+     * @param string $categorie
+     * @return ReferenceFiltre
+     */
+    public function setCategorie($categorie)
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * Get categorie
+     *
+     * @return string 
+     */
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+
+    /**
+     * Set categoriefiltre
+     *
+     * @param integer $categoriefiltre
+     * @return ReferenceFiltre
+     */
+    public function setCategoriefiltre($categoriefiltre)
+    {
+        $this->categoriefiltre = $categoriefiltre;
+
+        return $this;
+    }
+
+    /**
+     * Get categoriefiltre
+     *
+     * @return integer 
+     */
+    public function getCategoriefiltre()
+    {
+        return $this->categoriefiltre;
+    }
+
+    /**
+     * Set createur
+     *
+     * @param string $createur
+     * @return ReferenceFiltre
+     */
+    public function setCreateur($createur)
+    {
+        $this->createur = $createur;
+
+        return $this;
+    }
+
+    /**
+     * Get createur
+     *
+     * @return string 
+     */
+    public function getCreateur()
+    {
+        return $this->createur;
+    }
+
+    /**
+     * Set createurfiltre
+     *
+     * @param integer $createurfiltre
+     * @return ReferenceFiltre
+     */
+    public function setCreateurfiltre($createurfiltre)
+    {
+        $this->createurfiltre = $createurfiltre;
+
+        return $this;
+    }
+
+    /**
+     * Get createurfiltre
+     *
+     * @return integer 
+     */
+    public function getCreateurfiltre()
+    {
+        return $this->createurfiltre;
+    }
+}
